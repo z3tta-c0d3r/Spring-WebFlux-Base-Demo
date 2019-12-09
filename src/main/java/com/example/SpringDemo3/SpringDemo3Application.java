136a5c2749e357296cd303751eb1e0c1e939a557
z3tta-c0d3r <57172461+z3tta-c0d3r@users.noreply.github.com>
2019-12-09 22:25:46 +0100

    Add files via upload

---
+++ src/main/java/com/example/SpringDemo3/SpringDemo3Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringDemo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemo3Application.class, args);
+	}
+
+}
